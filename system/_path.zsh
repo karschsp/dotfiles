c4c5e13ad43cc8b83d64c79b27834d5fdcf5d219
Steve Karsch <steve.karsch@rodale.com>
2015-07-31 15:28:07 -0400

    Add some aliases and some symlinks.

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:/usr/local/mysql-5.6.25-osx10.8-x86_64/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,3 @@
 alias reload!='. ~/.zshrc'
+alias nano='vim'
+alias fix_drupal_perms='find . -type d -name files -exec chmod 777 {} \;'
