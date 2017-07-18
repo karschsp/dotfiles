8bd7904f57ccf92702bb24921d3fc33c639bbcef
Steve Karsch <steve.karsch@rodale.com>
2017-07-18 15:27:22 -0400

    Add gbr alias to show dates of git branches.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -17,3 +17,4 @@ alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gbr="git for-each-ref --sort=committerdate refs/heads/ --format='%(committerdate:short) %(refname:short)'"
